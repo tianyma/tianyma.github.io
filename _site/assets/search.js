076607982358b01ae657a306bc5217eb7bd40d56
himarsmty <1584666784@qq.com>
2020-11-11 11:52:59 +0800

    add jekyll-text-theme

---
+++ _site/assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Welcome to Jekyll!",'url':"/jekyll/update/2020/11/11/welcome-to-jekyll.html"}]};
