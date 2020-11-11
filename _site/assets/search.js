8ec3da05d1170d6c2db84fef6ad86e9a524cde7a
himarsmty <1584666784@qq.com>
2020-11-11 15:43:21 +0800

    add first blog

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Articles - Item (Excerpt + Read More + Info)",'url':"/"},{'title':"写博客的意义是什么？",'url':"/"},{'title':"写博客的意义是什么？12",'url':"/"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"写博客的意义是什么？",'url':"/2020/11/11/%E5%86%99%E5%8D%9A%E5%AE%A2%E7%9A%84%E6%84%8F%E4%B9%89%E6%98%AF%E4%BB%80%E4%B9%88.html"}]};
